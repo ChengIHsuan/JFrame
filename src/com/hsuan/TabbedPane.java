27a78f3505096d6f148edad9097fec146957775f
ChengIHsuan <cosine12621@gmail.com>
2018-09-18 17:04:20 +0800

    0918_TabbedPane

--- src/com/hsuan/TabbedPane.java
+++ src/com/hsuan/TabbedPane.java
@@ -5,10 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
 
 public class TabbedPane {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -40,18 +45,32 @@ public class TabbedPane {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 712, 490);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(15, 15, 660, 404);
 		frame.getContentPane().add(tabbedPane);
 		
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Button", null, panel, null);
 		panel.setLayout(null);
 		
-		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("Tabel", null, panel_1, null);
-		panel_1.setLayout(null);
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(36, 35, 111, 31);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(176, 35, 111, 31);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		btnNewButton_2.setBounds(321, 35, 111, 31);
+		panel.add(btnNewButton_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		tabbedPane.addTab("Tabel", null, scrollPane, null);
+		
+		TabelObject to = new TabelObject();
+		table = new JTable(to.data, to.columnNames);
+		scrollPane.setViewportView(table);
 	}
 }
---
+++ src/com/hsuan/TabelObject.java
@@ -0,0 +1,10 @@
+package com.hsuan;
+
+public class TabelObject {
+	
+	String[] columnNames = {"Name", "Birthday", "BloodType", "Phone"};
+	Object[][] data = {
+			{"IHsuan", "12/06", "O", "0926775092"},
+			{"Iris", "12/19", "C", "0912345678"}
+	};
+}
