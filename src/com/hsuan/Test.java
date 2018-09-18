af490d61308904387480417e3f0a1205b4fec6c5
ChengIHsuan <cosine12621@gmail.com>
2018-09-18 16:06:12 +0800

    initial commit

---
+++ src/com/hsuan/Test.java
@@ -0,0 +1,16 @@
+package com.hsuan;
+
+import java.lang.*;
+import java.awt.*;
+
+public class Test extends Frame {
+
+
+	public static void main(String[] args) {
+		Frame frm1 = new Frame();
+		frm1.setTitle("title");
+		frm1.setSize(600, 400);
+		frm1.setVisible(true);
+	}
+
+}
